fdc1264d7d462f17adbb0dae96bd5d5519f79225
negarkiran <negarkiran@gmail.com>
2018-06-14 14:07:24 +0530

    Create RotateArray.java

---
+++ arrays/RotateArray.java
@@ -0,0 +1,21 @@
+package com.algo.arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+	public static void main(String[] args) {
+		int[] intArray = {1,2,3,4,5,6};
+		int[] tempArray = new int[intArray.length];
+		int n=3;
+		for(int i=0;i<intArray.length-n;i++){
+			tempArray[i] = intArray[i+n];
+		}
+		int count = n-1;
+		for(int j=tempArray.length-1;j>=tempArray.length-n;j--){
+			tempArray[j] = intArray[count--];
+		}
+		Arrays.stream(tempArray).forEach(System.out::println);
+	}
+
+}
